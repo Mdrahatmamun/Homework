bd946e67b95ddf313da0cb3cd384425b8c0ae525
mdrahatmamun <mdrhtlmmn@gmail.com>
2022-05-15 21:21:11 -0400

    homework

---
+++ src/hw1JavaBasics/MySelf01.java
@@ -0,0 +1,22 @@
+package hw1JavaBasics;
+
+public class MySelf01 {
+
+	/*
+	 * Naming convention: Project name start with UpperCase(MySelf01) Class name
+	 * start with the UpperCase (MySelf01) Package name generally starts with
+	 * lowercase (hw1JavaBasic)
+	 * CamelCase example (April2022CoreJava)
+	 * SnakeCase example (Naming_convention)
+	 * 
+	 */
+
+	public static void main(String[] args) {
+
+		// Main method is an entry point of java.
+
+		System.out.println("Md Rahat Mamun");
+		System.out.println("145 chester ave apt d3 brooklyn NY11218");
+
+	}
+}
---
+++ src/hw2JavaBasics/MyCar.java
@@ -0,0 +1,18 @@
+
+package hw2JavaBasics; // I select source and  I go header to click New Java Package. 
+
+public class MyCar { // I select on package and I go header to click new java class.
+
+	public static void main(String[] args) {
+
+		System.out.println("My Name is \''MD Rahat Al Mamun\'', ");
+		// I copy syso by command ctrl+alt+down arrow
+		System.out.print(" I love to drive," + " My car info is below");
+		System.out.print(" \n Toyota Highlander Hybrid. (made in USA)  \n " + "Model- 2021\n " + "Color BK\t(4WD) ");
+
+// I organize code ate the end by press  ctrl + a by select all and then shift+ctrl + f reorganize the codes of editor.
+// At last i click ctrl + a to select all and then copy ctrl+c to past and submit my HW.
+
+	}
+
+}
