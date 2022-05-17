359f82824c5a77afbf38b8eb833272cd36ecd64c
mdrahatmamun <mdrhtlmmn@gmail.com>
2022-05-16 22:40:34 -0400

    home work4

---
+++ src/hw4Q2JavaVariables/AboutMe.java
@@ -0,0 +1,31 @@
+package hw4Q2JavaVariables;
+/*
+Create a package name "hw4JavaVariables" inside the HW project. Create a Class name "AboutMe". Declare String and other 8 types of variable. Create a constructor and Give a sysout inside Constructor which will print "This is all about us: ". Then Create a method name aboutMe. Inside the method -use String and other 8 types of variables inside sysout to define a person's meaningful info. Create another class AboutMeTest. Instantiate AboutMe class under main method. Initialize variables and call the method by object. I expect print outcome for 2 person, one for you and another for your best friend Alex. give a single line comment where a variable is declared and initialized, constructor is declared and initialized, method is implemented and initialized. Follow indentation by Organizing the code and paste below. [Mark distribution (125) : using variable --10, Create a method and constructor - 20, instantiate class - 10, initialize variables and call methods for you and Alex= 20,  variable is declared and initialized, constructor is declared and initialized, method is implemented and initialized -- 15, organize code and other-- 30]. push your code to github -- 20. share the package link below.
+125 points
+
+ */
+public class AboutMe {
+	// variable is declared here
+	public String name; 
+	public byte age; 
+	public short myAparmentRent ; 
+	public int myYearlySalary; 
+	public long myBankBalance; 
+	public float myHeight; 
+	public double myGrade; 
+	public char sex; 
+	public boolean usCitizen;
+  
+	// Contractor initialize.
+	public AboutMe() {
+		System.out.println("This is a constractor");
+	}
+	public void aboutme() { //This is a methos. method name start with lower case.
+		
+	}
+	
+	
+	
+
+
+}
